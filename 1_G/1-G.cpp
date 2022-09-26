a625fd58ec222e788f33c042585d5e1dcd22f882
nvclon <nvclon@github.com>
2022-09-26 21:02:45 +0300

    Initial_commit

--- 1_G/1-G.cpp
+++ 1_G/1-G.cpp
@@ -3,18 +3,18 @@
 #include <iostream>
 std::pair<long double, int> Simplify(long double x) {
   if (x > 1) {
-    return { Simplify(x / 10).first, Simplify(x / 10).second + 1 };
+    return {Simplify(x / 10).first, Simplify(x / 10).second + 1};
   }
   if (x <= 0.1) {
-    return { Simplify(x * 10).first, Simplify(x * 10).second - 1 };
+    return {Simplify(x * 10).first, Simplify(x * 10).second - 1};
   }
-    return { x, 0 };
+    return {x, 0};
 }
 long double FindGeoAvgOfElements(std::pair<long double, int>* prefix, int l, int r) {
   std::pair<long double, int> temp_pair = Simplify(prefix[r].first / prefix[l - 1].first);
-  std::pair<long double, int> multiplication = { temp_pair.first,
-                                                 temp_pair.second + prefix[r].second - prefix[l - 1].second
-                                               };
+  std::pair<long double, int> multiplication = {
+      temp_pair.first,
+      temp_pair.second + prefix[r].second - prefix[l - 1].second};
   long double power = static_cast<long double>(1) / (r - l + 1);
   return std::pow(multiplication.first, power) *
          std::pow(10, multiplication.second * power);
@@ -22,7 +22,7 @@ long double FindGeoAvgOfElements(std::pair<long double, int>* prefix, int l, int
 int main() {
   std::cout.precision(16);
   int n;
-  std::pair<long double, int>* prefix = new std::pair<long double, int>[300001];
+  std::pair<long double, int> *prefix = new std::pair<long double, int>[300001];
   std::cin >> n;
   prefix[0] = {1, 0};
   for (int i = 0; i < n; i++) {
